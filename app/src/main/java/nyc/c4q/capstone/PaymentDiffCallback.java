fcd9dbd5bf5d07ecd766115f2c4c02e1e0843225
Jervon Arnold <jervonjarnold@gmail.com>
2018-04-01 12:44:21 -0400

    added rent payment to data base and minor fixes with handling nullpointer

---
+++ app/src/main/java/nyc/c4q/capstone/PaymentDiffCallback.java
@@ -0,0 +1,59 @@
+package nyc.c4q.capstone;
+
+import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+
+import nyc.c4q.capstone.datamodels.PaymentHistoryModel;
+import nyc.c4q.capstone.datamodels.Tickets;
+
+/**
+ * Created by jervon.arnoldd on 4/1/18.
+ */
+
+public class PaymentDiffCallback extends DiffUtil.Callback {
+
+
+    private final List<PaymentHistoryModel> oldRentList;
+    private final List<PaymentHistoryModel> newRentList;
+
+    public PaymentDiffCallback(List<PaymentHistoryModel> oldEmployeeList, List<PaymentHistoryModel> newEmployeeList) {
+        this.oldRentList = oldEmployeeList;
+        this.newRentList = newEmployeeList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldRentList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newRentList.size();
+    }
+
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldRentList.get(oldItemPosition).getConf() == newRentList.get(
+                newItemPosition).getConf();
+    }
+
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        final PaymentHistoryModel oldPayment = oldRentList.get(oldItemPosition);
+        final PaymentHistoryModel newPayment = newRentList.get(newItemPosition);
+        return oldPayment.getAmount_paid().equals(newPayment.getAmount_paid());
+    }
+
+
+    @Nullable
+    @Override
+    public Object getChangePayload(int oldItemPosition, int newItemPosition) {
+        // Implement method if you're going to use ItemAnimator
+        return super.getChangePayload(oldItemPosition, newItemPosition);
+    }
+}
+
--- app/src/main/java/nyc/c4q/capstone/payment_history_package/PaymentHistoryAdapter.java
+++ app/src/main/java/nyc/c4q/capstone/payment_history_package/PaymentHistoryAdapter.java
@@ -1,33 +1,43 @@
 package nyc.c4q.capstone.payment_history_package;
 
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import nyc.c4q.capstone.PaymentDiffCallback;
 import nyc.c4q.capstone.R;
+import nyc.c4q.capstone.TicketsDiffCallback;
 import nyc.c4q.capstone.datamodels.PaymentHistoryModel;
+import nyc.c4q.capstone.datamodels.Tickets;
 
 /**
  * Created by D on 3/18/18.
  */
 
-public class PaymentHistoryAdapter extends RecyclerView.Adapter<PaymentHistoryHolder>{
-
+public class PaymentHistoryAdapter extends RecyclerView.Adapter<PaymentHistoryHolder> {
 
 
     private List<PaymentHistoryModel> paymentModelList;
 
     public PaymentHistoryAdapter(List<PaymentHistoryModel> paymentModelList) {
-        this.paymentModelList = paymentModelList;
+        if (paymentModelList == null) {
+            this.paymentModelList = new ArrayList<>();
+        } else {
+            this.paymentModelList = paymentModelList;
+        }
     }
 
+    public PaymentHistoryAdapter() {
+    }
 
     @Override
     public PaymentHistoryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_itemview_layout,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_itemview_layout, parent, false);
         return new PaymentHistoryHolder(view);
     }
 
@@ -36,6 +46,15 @@ public class PaymentHistoryAdapter extends RecyclerView.Adapter<PaymentHistoryHo
 
         PaymentHistoryModel paymentModel = paymentModelList.get(position);
         holder.onBind(paymentModel);
+    }
+
+
+    public void updateTicketListItems(List<PaymentHistoryModel> payment) {
+        final PaymentDiffCallback diffCallback = new PaymentDiffCallback(this.paymentModelList, payment);
+        final DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(diffCallback);
+        this.paymentModelList.clear();
+        this.paymentModelList.addAll(payment);
+        diffResult.dispatchUpdatesTo(this);
     }
 
     @Override
